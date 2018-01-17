cb0f4daa2d422eabf576a42b46d2215b0a5fa612
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-01-17 08:51:10 +0000

    Preparing 2.4.1 : update trunk modules versions
    SVN:trunk[6389]

--- module.precanned-replies-pro.php
+++ module.precanned-replies-pro.php
@@ -3,7 +3,7 @@
 //
 //   This file is part of iTop.
 //
-//   iTop is free software; you can redistribute it and/or modify	
+//   iTop is free software; you can redistribute it and/or modify
 //   it under the terms of the GNU Affero General Public License as published by
 //   the Free Software Foundation, either version 3 of the License, or
 //   (at your option) any later version.
@@ -23,7 +23,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'precanned-replies-pro/1.0.2',
+	'precanned-replies-pro/1.1.0',
 	array(
 		// Identification
 		//
@@ -44,7 +44,7 @@ SetupWebPage::AddModule(
 			'model.precanned-replies-pro.php',
 		),
 		'webservice' => array(
-			
+
 		),
 		'data.struct' => array(
 			// add your 'structure' definition XML files here,
@@ -52,11 +52,11 @@ SetupWebPage::AddModule(
 		'data.sample' => array(
 			// add your sample data XML files here,
 		),
-		
+
 		// Documentation
 		//
 		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
-		'doc.more_information' => '', // hyperlink to more information, if any 
+		'doc.more_information' => '', // hyperlink to more information, if any
 
 		// Default settings
 		//
