c0273e323c65fac42c3bc784bbc69eb3147fe14b
Pierre Goiffon <pierre.goiffon@combodo.com>
2021-11-23 09:14:36 +0100

    :bookmark: Update module version from 1.0.7 to 1.0.8

--- module.precanned-replies-pro.php
+++ module.precanned-replies-pro.php
@@ -23,7 +23,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'precanned-replies-pro/1.0.7',
+	'precanned-replies-pro/1.0.8',
 	array(
 		// Identification
 		//
