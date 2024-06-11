1e6197cb5e4ee69e0537705df8daa07d3e4eb923
Timothee <timothee.callet@combodo.com>
2024-06-11 16:59:03 +0200

    Automated dictionaries synchronization with english reference

--- hu.dict.precanned-replies-pro.php
+++ hu.dict.precanned-replies-pro.php
@@ -2,40 +2,26 @@
 /**
  * Localized data
  *
- * @copyright Copyright (C) 2010-2018 Combodo SARL
- * @license	http://opensource.org/licenses/AGPL-3.0
- *
- * This file is part of iTop.
- *
- * iTop is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * iTop is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with iTop. If not, see <http://www.gnu.org/licenses/>
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license    https://opensource.org/licenses/AGPL-3.0
+ * 
  */
-Dict::Add('HU HU', 'Hungarian', 'Magyar', array(
-	'Menu:PrecannedRepliesCategories' => 'Precanned replies categories~~',
-	'Menu:PrecannedRepliesCategories+' => 'Precanned replies categories~~',
-
-	'Class:PrecannedReplyCategory' => 'Precanned reply category~~',
-	'Class:PrecannedReplyCategory+' => 'Precanned reply~~',
-	'Class:PrecannedReplyCategory/Attribute:name' => 'Name~~',
-	'Class:PrecannedReplyCategory/Attribute:name+' => '~~',
-	'Class:PrecannedReplyCategory/Attribute:description' => 'Description~~',
-	'Class:PrecannedReplyCategory/Attribute:description+' => '~~',
-	'Class:PrecannedReplyCategory/Attribute:replies_list' => 'Precanned Replies~~',
-	'Class:PrecannedReplyCategory/Attribute:replies_list+' => 'Precanned Replies in this category~~',
-
-	'Class:PrecannedReply/Attribute:org_id' => 'Organization~~',
-	'Class:PrecannedReply/Attribute:org_id+' => '~~',
+/**
+ *
+ */
+Dict::Add('HU HU', 'Hungarian', 'Magyar', [
 	'Class:PrecannedReply/Attribute:category_id' => 'Category~~',
 	'Class:PrecannedReply/Attribute:category_id+' => '~~',
-));
-
+	'Class:PrecannedReply/Attribute:org_id' => 'Organization~~',
+	'Class:PrecannedReply/Attribute:org_id+' => '~~',
+	'Class:PrecannedReplyCategory' => 'Precanned reply category~~',
+	'Class:PrecannedReplyCategory+' => 'Precanned reply~~',
+	'Class:PrecannedReplyCategory/Attribute:description' => 'Description~~',
+	'Class:PrecannedReplyCategory/Attribute:description+' => '~~',
+	'Class:PrecannedReplyCategory/Attribute:name' => 'Name~~',
+	'Class:PrecannedReplyCategory/Attribute:name+' => '~~',
+	'Class:PrecannedReplyCategory/Attribute:replies_list' => 'Precanned Replies~~',
+	'Class:PrecannedReplyCategory/Attribute:replies_list+' => 'Precanned Replies in this category~~',
+	'Menu:PrecannedRepliesCategories' => 'Precanned replies categories~~',
+	'Menu:PrecannedRepliesCategories+' => 'Precanned replies categories~~',
+]);
